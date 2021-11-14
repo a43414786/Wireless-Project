134fe4fe008b98fc863e468ab4c6ccc5b02a0e40
a43414786 <a43414786@gmail.com>
2021-11-15 00:36:37 +0800

    k

--- wlproject.py
+++ wlproject.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 import time
-
+import pickle
 from numpy import random
 
 class car:
@@ -37,51 +37,82 @@ class map:
                 self.img[point[0]-i][point[1]+j] = color
                 self.img[point[0]-i][point[1]-j] = color
 
-    def point_clearh(self,point,size):
-        for i in range(size//2):
-            for j in range(size//2):
-                if i != 0:
-                    self.img[point[0]+i][point[1]+j] = [0,0,0]
-                    self.img[point[0]+i][point[1]-j] = [0,0,0]
-                    self.img[point[0]-i][point[1]+j] = [0,0,0]
-                    self.img[point[0]-i][point[1]-j] = [0,0,0]
-                else:
-                    self.img[point[0]+i][point[1]+j] = [255,255,255]
-                    self.img[point[0]+i][point[1]-j] = [255,255,255]
-                    self.img[point[0]-i][point[1]+j] = [255,255,255]
-                    self.img[point[0]-i][point[1]-j] = [255,255,255]
-
-    def point_clearv(self,point,size):
-        for i in range(size//2):
-            for j in range(size//2):
-                if j != 0:
-                    self.img[point[0]+i][point[1]+j] = [0,0,0]
-                    self.img[point[0]+i][point[1]-j] = [0,0,0]
-                    self.img[point[0]-i][point[1]+j] = [0,0,0]
-                    self.img[point[0]-i][point[1]-j] = [0,0,0]
-                else:
-                    self.img[point[0]+i][point[1]+j] = [255,255,255]
-                    self.img[point[0]+i][point[1]-j] = [255,255,255]
-                    self.img[point[0]-i][point[1]+j] = [255,255,255]
-                    self.img[point[0]-i][point[1]-j] = [255,255,255]
-
     def change_dir(self,i):
-        dir = np.random.randint(0,4)
-        if dir == 0:
-            i.direct = [0,10]
-            i.hv = 0
-        elif dir == 1:
-            i.direct = [0,-10]
-            i.hv = 0
-        elif dir == 2:
-            i.direct = [10,0]
-            i.hv = 1
+        dir = np.random.randint(0,32)    
+        if i.hv == 0:
+            if dir <= 15:   #straight
+                i.direct = [0,10]
+                i.hv = 0
+            elif 16 <= dir <= 17:   #back
+                i.direct = [0,-10]
+                i.hv = 1
+            elif 18 <= dir <= 24:   #right
+                i.direct = [10,0]
+                i.hv = 2
+            else:   #left
+                i.direct = [-10,0]
+                i.hv = 3
+        elif i.hv == 1:
+            if dir <= 15:   #straight
+                i.direct = [0,-10]
+                i.hv = 1
+            elif 16 <= dir <= 17:   #back
+                i.direct = [0,10]
+                i.hv = 0
+            elif 18 <= dir <= 24:   #right
+                i.direct = [-10,0]
+                i.hv = 3
+            else:   #left
+                i.direct = [10,0]
+                i.hv = 2            
+        elif i.hv == 2:
+            if dir <= 15:   #straight
+                i.direct = [10,0]
+                i.hv = 2
+            elif 16 <= dir <= 17:   #back
+                i.direct = [-10,0]
+                i.hv = 3
+            elif 18 <= dir <= 24:   #right
+                i.direct = [0,-10]
+                i.hv = 1
+            else:   #left
+                i.direct = [0,10]
+                i.hv = 0
         else:
-            i.direct = [-10,0]
-            i.hv = 1
+            if dir <= 15:   #straight
+                i.direct = [-10,0]
+                i.hv = 3
+            elif 16 <= dir <= 17:   #back
+                i.direct = [10,0]
+                i.hv = 2
+            elif 18 <= dir <= 24:   #right
+                i.direct = [0,10]
+                i.hv = 0
+            else:   #left
+                i.direct = [0,-10]
+                i.hv = 1    
 
     def update(self):
 
+        self.addcar()
+
+        with open("a.pickle" , "rb") as f:
+            self.img = pickle.load(f)
+
+        for index,i in enumerate(self.cars):
+
+            self.point_show(i.point,self.car_size,self.car_color)
+            
+            i.update_position()
+            
+            if (i.point[0] < 10) or (i.point[0] > 910) or (i.point[1] < 10) or (i.point[1] > 910):
+                self.cars.pop(index)
+                continue
+
+            if (((i.point[0]-10) % self.block_size) == 0) and (((i.point[1]-10) % self.block_size) == 0):
+                self.change_dir(i)
+                
+    def addcar(self):
         if  np.random.randint(0,12) == 0:
             i = np.random.randint(0,10)
             direct = [0,0]
@@ -92,44 +123,22 @@ class map:
                 hv = 0
             elif dir == 1:
                 direct = [0,-10]
-                hv = 0
+                hv = 1
             elif dir == 2:
                 direct = [10,0]
-                hv = 1
+                hv = 2
             else:
                 direct = [-10,0]
-                hv = 1
+                hv = 3
             if (i == 0) or (i == 9):
                 j = np.random.randint(0,10)
-                self.addcar([i*self.block_size + 10,j*self.block_size + 10],direct,hv)
+                self.cars.append(car([i*self.block_size + 10,j*self.block_size + 10],direct,hv))
             else:
                 j = np.random.randint(0,2)
                 if j == 1:
                     j=9
-                self.addcar([i*self.block_size + 10,j*self.block_size + 10],direct,hv)
-            
-
-        for index,i in enumerate(self.cars):
-
-            if i.hv == 0:
-                self.point_clearh(i.point,self.car_size)
-            else:
-                self.point_clearv(i.point,self.car_size)
-
-            i.update_position()
-            
-            if (i.point[0] < 10) or (i.point[0] > 910) or (i.point[1] < 10) or (i.point[1] > 910):
-                self.cars.pop(index)
-                continue
-
-            if (((i.point[0]-10) % self.block_size) == 0) and (((i.point[1]-10) % self.block_size) == 0):
-                self.change_dir(i)
-                
-            self.point_show(i.point,self.car_size,self.car_color)
-
-    def addcar(self,position,dir,hv):
-        self.cars.append(car(position,dir,hv))
-
+                self.cars.append(car([i*self.block_size + 10,j*self.block_size + 10],direct,hv))
+        
     def create_base(self,img_size):
         for i in range(10,img_size[0]-10):
             for j in range(10,img_size[1]-10):
@@ -161,6 +170,9 @@ class map:
                     self.img[i][j] = [255,255,255]
         
         self.create_base(img_size)
+        with open("a.pickle" , "wb") as f:
+            pickle.dump(self.img,f)
+
         
                             
         
@@ -173,7 +185,7 @@ def main():
     while 1:
         img.update()
         cv2.imshow("img",img.img)
-        time.sleep(0.05)
+        time.sleep(0.001)
         cv2.waitKey(1)
 
 
