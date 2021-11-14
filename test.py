c36779de7759cd0fe3fc7b07825672e7a7e60213
a43414786 <a43414786@gmail.com>
2021-11-14 18:18:04 +0800

    k

---
+++ test.py
@@ -0,0 +1,181 @@
+import cv2
+import numpy as np
+import time
+
+from numpy import random
+
+class car:
+    def __init__(self,position,dir,hv):
+        self.point = position
+        self.direct = dir
+        self.hv = hv
+    def update_position(self):
+        self.point[0] = self.point[0] + self.direct[0]
+        self.point[1] = self.point[1] + self.direct[1]
+
+class base:
+    def __init__(self,position):
+        self.point = position
+
+
+class map:
+    def __init__(self):
+        self.time = time.time()
+        self.base_size = 11
+        self.car_size = 7
+        self.base_color = [255,0,200] 
+        self.car_color = [255,200,0] 
+        self.bases = []
+        self.cars = []
+        self.block_size = 100
+        self.img = 0
+    def point_show(self,point,size,color):
+        for i in range(size//2):
+            for j in range(size//2):
+                self.img[point[0]+i][point[1]+j] = color
+                self.img[point[0]+i][point[1]-j] = color
+                self.img[point[0]-i][point[1]+j] = color
+                self.img[point[0]-i][point[1]-j] = color
+
+    def point_clearh(self,point,size):
+        for i in range(size//2):
+            for j in range(size//2):
+                if i != 0:
+                    self.img[point[0]+i][point[1]+j] = [0,0,0]
+                    self.img[point[0]+i][point[1]-j] = [0,0,0]
+                    self.img[point[0]-i][point[1]+j] = [0,0,0]
+                    self.img[point[0]-i][point[1]-j] = [0,0,0]
+                else:
+                    self.img[point[0]+i][point[1]+j] = [255,255,255]
+                    self.img[point[0]+i][point[1]-j] = [255,255,255]
+                    self.img[point[0]-i][point[1]+j] = [255,255,255]
+                    self.img[point[0]-i][point[1]-j] = [255,255,255]
+
+    def point_clearv(self,point,size):
+        for i in range(size//2):
+            for j in range(size//2):
+                if j != 0:
+                    self.img[point[0]+i][point[1]+j] = [0,0,0]
+                    self.img[point[0]+i][point[1]-j] = [0,0,0]
+                    self.img[point[0]-i][point[1]+j] = [0,0,0]
+                    self.img[point[0]-i][point[1]-j] = [0,0,0]
+                else:
+                    self.img[point[0]+i][point[1]+j] = [255,255,255]
+                    self.img[point[0]+i][point[1]-j] = [255,255,255]
+                    self.img[point[0]-i][point[1]+j] = [255,255,255]
+                    self.img[point[0]-i][point[1]-j] = [255,255,255]
+
+    def change_dir(self,i):
+        dir = np.random.randint(0,4)
+        if dir == 0:
+            i.direct = [0,10]
+            i.hv = 0
+        elif dir == 1:
+            i.direct = [0,-10]
+            i.hv = 0
+        elif dir == 2:
+            i.direct = [10,0]
+            i.hv = 1
+        else:
+            i.direct = [-10,0]
+            i.hv = 1
+
+    def update(self):
+
+        if  np.random.randint(0,12) == 0:
+            i = np.random.randint(0,10)
+            direct = [0,0]
+            hv = 0
+            dir = np.random.randint(0,4)
+            if dir == 0:
+                direct = [0,10]
+                hv = 0
+            elif dir == 1:
+                direct = [0,-10]
+                hv = 0
+            elif dir == 2:
+                direct = [10,0]
+                hv = 1
+            else:
+                direct = [-10,0]
+                hv = 1
+            if (i == 0) or (i == 9):
+                j = np.random.randint(0,10)
+                self.addcar([i*self.block_size + 10,j*self.block_size + 10],direct,hv)
+            else:
+                j = np.random.randint(0,2)
+                if j == 1:
+                    j=9
+                self.addcar([i*self.block_size + 10,j*self.block_size + 10],direct,hv)
+            
+
+        for index,i in enumerate(self.cars):
+
+            if i.hv == 0:
+                self.point_clearh(i.point,self.car_size)
+            else:
+                self.point_clearv(i.point,self.car_size)
+
+            i.update_position()
+            
+            if (i.point[0] < 10) or (i.point[0] > 910) or (i.point[1] < 10) or (i.point[1] > 910):
+                self.cars.pop(index)
+                continue
+
+            if (((i.point[0]-10) % self.block_size) == 0) and (((i.point[1]-10) % self.block_size) == 0):
+                self.change_dir(i)
+                
+            self.point_show(i.point,self.car_size,self.car_color)
+
+    def addcar(self,position,dir,hv):
+        self.cars.append(car(position,dir,hv))
+
+    def create_base(self,img_size):
+        for i in range(10,img_size[0]-10):
+            for j in range(10,img_size[1]-10):
+                if ((i-10) % (self.block_size/2) == 0) and ((i-10) % self.block_size != 0):
+                    if ((j-10) % (self.block_size/2) == 0) and ((j-10) % self.block_size != 0):
+                        if np.random.randint(0,10) == 0:
+                            pos = np.random.randint(0,4)
+                            if pos == 0:
+                                self.point_show((i+10,j),self.base_size,self.base_color)
+                                self.bases.append(base((i+10,j)))
+                            elif pos == 1:
+                                self.point_show((i-10,j),self.base_size,self.base_color)
+                                self.bases.append(base((i-10,j)))
+                            elif pos == 2:
+                                self.point_show((i,j+10),self.base_size,self.base_color)
+                                self.bases.append(base((i,j+10)))
+                            else:
+                                self.point_show((i,j-10),self.base_size,self.base_color)
+                                self.bases.append(base((i,j-10)))
+
+    def makemap(self):
+        img_size = (self.block_size*9+21,self.block_size*9+21,3)
+        self.img = np.zeros(img_size)
+        for i in range(10,img_size[0]-10):
+            for j in range(10,img_size[1]-10):
+                if (i-10) % self.block_size == 0:
+                    self.img[i][j] = [255,255,255]
+                if (j-10) % self.block_size == 0:
+                    self.img[i][j] = [255,255,255]
+        
+        self.create_base(img_size)
+        
+                            
+        
+
+def main():
+    img = map()
+    img.makemap()
+    
+    cv2.namedWindow("img")
+    while 1:
+        img.update()
+        cv2.imshow("img",img.img)
+        #time.sleep(1)
+        cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    main()
